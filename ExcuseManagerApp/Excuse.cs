ea63dcaa16330abb59cd94f84c6f792f53353b7e
Quy Nguyen <trongquy267@gmail.com>
2016-03-27 23:56:53 +0700

    version 1.0

---
+++ ExcuseManagerApp/Excuse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ExcuseManagerApp.Annotations;
+
+namespace ExcuseManagerApp
+{
+    class Excuse : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string LastUsed { get; set; }
+        public string Description { get; set; }
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChangedEvent = PropertyChanged;
+            if (propertyChangedEvent != null)
+            {
+                propertyChangedEvent(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
